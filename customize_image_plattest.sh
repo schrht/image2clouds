1a27bd32e9227c8b17427d72bbbf3c3a98169513
Charles Shih <shichen120@gmail.com>
2020-02-03 20:11:04 +0800

    Bugfix for image file name

--- customize_image_plattest.sh
+++ customize_image_plattest.sh
@@ -4,7 +4,8 @@
 #   Customize $IMAGE_FILE for platform testing usage.
 #
 # History:
-#   v1.0  2020-02-03  charles.shih  Init version
+#   v1.0    2020-02-03  charles.shih  Init version
+#   v1.0.1  2020-02-03  charles.shih  Bugfix for image file name
 
 # Load profile and verify the veribles
 source ./profile
@@ -16,11 +17,11 @@ virt-customize -V >/dev/null || exit 1
 
 # Enlarge the image
 echo -e "\nEnlarge the image to 20 GiB..."
-fsize=$(ls -l $WORKSPACE/$IMAGE_FILE | awk '{print $5}')
+fsize=$(ls -l $IMAGE_FILE | awk '{print $5}')
 if [ "$fsize" -lt "$((20 * 1024 * 1024 * 1024))" ]; then
 	qemu-img create -f qcow2 -o preallocation=metadata $WORKSPACE/newdisk.qcow2 20G || exit 1
-	virt-resize --expand /dev/sda1 $WORKSPACE/$IMAGE_FILE $WORKSPACE/newdisk.qcow2 || exit 1
-	mv -f $WORKSPACE/newdisk.qcow2 $WORKSPACE/$IMAGE_FILE || exit 1
+	virt-resize --expand /dev/sda1 $IMAGE_FILE $WORKSPACE/newdisk.qcow2 || exit 1
+	mv -f $WORKSPACE/newdisk.qcow2 $IMAGE_FILE || exit 1
 else
 	echo -e "Already enlarged to 20 GiB, skip this operation."
 fi
@@ -39,11 +40,11 @@ read -t 30 -p "Run 'git pull' for repo \"linus\" [y/N]? (30s) " answer
 [ "$answer" = "y" ] && bash -c "cd ./linus && git pull" || echo
 
 echo -e "\nPlacing git repos..."
-virt-customize -a $WORKSPACE/$IMAGE_FILE --copy-in ./platform-test:/root/
-virt-customize -a $WORKSPACE/$IMAGE_FILE --copy-in ./linus:/root/
+virt-customize -a $IMAGE_FILE --copy-in ./platform-test:/root/
+virt-customize -a $IMAGE_FILE --copy-in ./linus:/root/
 
 # Reset SELinux label
 echo -e "\nResetting SELinux label..."
-virt-customize -a $WORKSPACE/$IMAGE_FILE --selinux-relabel
+virt-customize -a $IMAGE_FILE --selinux-relabel
 
 exit 0
