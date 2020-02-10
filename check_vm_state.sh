0e4d2c384e22ffd359d42dd6fa21d82cf4b8a271
Charles Shih <shichen120@gmail.com>
2020-02-10 19:45:21 +0800

    Add check_vm_state.sh

---
+++ check_vm_state.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Description:
+#   Check the state of VM.
+#
+# History:
+#   v1.0  2020-02-10  charles.shih  Init version
+
+# Parse parameters
+if [ "$1" != "" ] && [ "$1" != "running" ] && [ "$1" != "shutoff" ] && [ "$1" != "undefined" ]; then
+	echo -e "Usage:\n$0 <running|shutoff|undefined>"
+	exit 1
+fi
+
+# Load profile and verify the veribles
+source ./profile
+
+if [ ! -z "$DOMAIN_NAME" ]; then
+	# Get sudo access
+	sudo bash -c : || exit 1
+
+	# Check utilities
+	sudo virsh --version >/dev/null || exit 1
+
+	# Get VM state
+	state=$(sudo virsh list --all | grep -w "\s$DOMAIN_NAME\s" | awk '{print $3$4}')
+fi
+
+: ${state:=undefined}
+
+# Show VM state
+echo "Name: $DOMAIN_NAME  State: $state  Want: $1"
+
+[ "$state" = "$1" ] && exit 0 || exit 1
