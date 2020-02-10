34fc8a2305e0f5e0b47948475a82d47616466034
Charles Shih <shichen120@gmail.com>
2020-02-10 19:56:57 +0800

    Support running from anywhere.

--- i2c_platform_test.sh
+++ i2c_platform_test.sh
@@ -9,6 +9,7 @@
 # History:
 #   v1.0  2020-02-05  charles.shih  Init version
 #   v1.1  2020-02-10  Update ALIYUN_BUCKET and ALIYUN_REGION in profile
+#   v1.2  2020-02-10  Support running from anywhere
 
 set -e
 
@@ -18,24 +19,24 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
-./create_profile.sh $1
-./update_profile.sh ALIYUN_IMAGE_SIZE 100
-./update_profile.sh ALIYUN_BUCKET rhel-platform
-./update_profile.sh ALIYUN_REGION us-east-1
+$(dirname $0)/create_profile.sh $1
+$(dirname $0)/update_profile.sh ALIYUN_IMAGE_SIZE 100
+$(dirname $0)/update_profile.sh ALIYUN_BUCKET rhel-platform
+$(dirname $0)/update_profile.sh ALIYUN_REGION us-east-1
 
-./download_image.sh
-./process_image.sh
-./customize_platform_test.sh
+$(dirname $0)/download_image.sh
+$(dirname $0)/process_image.sh
+$(dirname $0)/customize_platform_test.sh
 
-./define_vm.sh
-./start_vm.sh
-./get_vm_info.sh
-./vm_execute.sh 'cd /root/platform-test/init && ./run.sh'
-./vm_execute.sh 'cd /root/platform-test && ./run.sh'
-./stop_vm.sh
-./undefine_vm.sh
+$(dirname $0)/define_vm.sh
+$(dirname $0)/start_vm.sh
+$(dirname $0)/get_vm_info.sh
+$(dirname $0)/vm_execute.sh 'cd /root/platform-test/init && ./run.sh'
+$(dirname $0)/vm_execute.sh 'cd /root/platform-test && ./run.sh'
+$(dirname $0)/stop_vm.sh
+$(dirname $0)/undefine_vm.sh
 
-./upload_image.sh
-./register_image.sh
+$(dirname $0)/upload_image.sh
+$(dirname $0)/register_image.sh
 
 exit 0
