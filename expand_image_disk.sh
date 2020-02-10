864ede08eeb45b6945cb236beff6d6c60c969e54
Charles Shih <shichen120@gmail.com>
2020-02-10 19:48:41 +0800

    Check VM state before executing.

--- expand_image_disk.sh
+++ expand_image_disk.sh
@@ -4,7 +4,8 @@
 #   Expand /dev/sda1 in $IMAGE_FILE.
 #
 # History:
-#   v1.0    2020-02-05  charles.shih  Move this logic out from another script.
+#   v1.0  2020-02-05  charles.shih  Move this logic out from another script.
+#   v1.1  2020-02-10  charles.shih  Check VM state before executing
 
 # Parse parameters
 if [ -z "$1" ]; then
@@ -20,6 +21,16 @@ source ./profile
 # Check utilities
 qemu-img -V >/dev/null || exit 1
 virt-resize -V >/dev/null || exit 1
+
+# Check VM state
+$(dirname $0)/check_vm_state.sh undefined
+if [ "$?" != "0" ]; then
+	$(dirname $0)/check_vm_state.sh shutoff
+	if [ "$?" != "0" ]; then
+		echo "ERROR: The VM must be stopped first."
+		exit 1
+	fi
+fi
 
 # Enlarge the image
 size=$1
